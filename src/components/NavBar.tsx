92a3d43840f92a3b7f50dc68d9b77fe0c8621655
Cedrick Catalan <ccata002@gmail.com>
2025-03-09 03:49:53 -0400

    Revert "fixed version number not appearing"
    This reverts commit 7bd0493d28b5b11c6dfda90cb2cd702841736e3f.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -48,7 +48,7 @@ export function NavBar() {
     const [businessDate] = useBusinessDate();
     const { businessDatePicker, showBusinessDatePicker } = useBusinessDatePicker();
     const location = useLocation();
-    const version = getEnv('VITE_REACT_APP_VERSION') || '?.?.?';
+    const version = getEnv('VITE_REACT_APP_VERSION');
     // const version = import.meta.env.VITE_REACT_APP_VERSION || process.env.VITE_REACT_APP_VERSION;
 
     const queryClient = useQueryClient();
--- src/utils.ts
+++ src/utils.ts
@@ -63,7 +63,12 @@ export const getEnv = (variableName: string): string => {
     if (variableName === 'MODE') {
         return import.meta.env.MODE || (process.env.NODE_ENV as string);
     }
-    return import.meta.env[variableName] || process.env[variableName];
+    if (import.meta.env.MODE === 'development') {
+        return import.meta.env[variableName] || process.env[variableName];
+    } else {
+        // In production, only check import.meta.env
+        return import.meta.env[variableName];
+    }
 };
 
 export const devOnly = (child: React.ReactElement) => {
