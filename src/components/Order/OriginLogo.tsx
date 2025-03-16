500bf148780d1c16d92cadd0a392ab45e005ad92
Cedrick Catalan <ccata002@gmail.com>
2025-03-15 23:55:30 -0400

    improved RoundLottieIcon

--- src/components/Order/OriginLogo.tsx
+++ src/components/Order/OriginLogo.tsx
@@ -1,18 +1,10 @@
 import { OrderOrigin } from '../../typesAndValidators';
-// import { RoundImage, RoundImageProps } from '../Base/RoundImage';
 import { RoundLottieIcon } from '../../rickcedlib/LottieIcons';
 
-// type OriginLogoProps = Omit<RoundImageProps, 'src' | 'alt'> & { orderOrigin: OrderOrigin };
 type OriginLogoProps = { orderOrigin: OrderOrigin };
 
 export const OriginLogo = ({ orderOrigin }: OriginLogoProps) => {
     return (
-        // <RoundImage
-        //     src={orderOrigin.icon || ''}
-        //     alt={orderOrigin.name}
-        //     className={`origin-logo-${orderOrigin.name}`}
-        //     {...props}
-        // />
         <RoundLottieIcon
             imageSrc={orderOrigin.icon || ''}
             height="25px"
