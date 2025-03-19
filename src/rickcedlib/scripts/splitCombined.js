49cc32d00cab15618b78328495139102e834bc1a
Cedrick Catalan <ccata002@gmail.com>
2025-03-19 15:20:35 -0400

    created scripts to help with paraglide messages

---
+++ src/rickcedlib/scripts/splitCombined.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+
+function splitMessagesJson() {
+    const combinedJSON = 'messages/combined.json';
+    const enPath = 'messages/en.json';
+    const ptPath = 'messages/pt.json';
+    const esPath = 'messages/es.json';
+    try {
+        const inputData = JSON.parse(fs.readFileSync(combinedJSON, 'utf8'));
+
+        const enData = {};
+        const ptData = {};
+        const esData = {};
+
+        Object.keys(inputData).forEach((key) => {
+            if (key !== '$schema') {
+                enData[key] = inputData[key].en || '';
+                ptData[key] = inputData[key].pt || '';
+                esData[key] = inputData[key].es || '';
+            }
+        });
+
+        fs.writeFileSync(enPath, JSON.stringify(enData, null, 2), 'utf8');
+        fs.writeFileSync(ptPath, JSON.stringify(ptData, null, 2), 'utf8');
+        fs.writeFileSync(esPath, JSON.stringify(esData, null, 2), 'utf8');
+    } catch (error) {
+        console.error('Error splitting locale JSONs:', error);
+    }
+}
+
+splitMessagesJson();
