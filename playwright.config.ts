5a60fae884b994781481b74349a68eeb55c9064e
Cedrick Catalan <ccata002@gmail.com>
2025-03-23 12:22:28 -0400

    added baseURL for production testing and adjusted overscroll behavior; fixed payment summations in OrderDashboard

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,29 @@
 import { defineConfig } from '@playwright/test';
 
+// npx playwright test --grep="dev"
+// npx playwright test --grep="prod"
+
 export default defineConfig({
     testDir: './tests', // Directory for E2E tests
     retries: 0, // Number of retries on failures
-    use: {
-        baseURL: 'http://localhost:5173', // URL of your app
-        // headless: true, // Set to false for debugging with the browser open
-        headless: false,
-        viewport: { width: 1280, height: 720 },
-        actionTimeout: 5000, // Timeout for each action
-    },
+    projects: [
+        {
+            name: 'dev',
+            use: {
+                baseURL: 'http://localhost:5173',
+                headless: false,
+                viewport: { width: 1280, height: 720 },
+                actionTimeout: 5000,
+            },
+        },
+        {
+            name: 'prod',
+            use: {
+                baseURL: 'https://app.bari.pizza',
+                headless: false,
+                viewport: { width: 1280, height: 720 },
+                actionTimeout: 5000,
+            },
+        },
+    ],
 });
